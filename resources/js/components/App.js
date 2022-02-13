e77db5f92c25790bbfb9cd823b322064e44ea176
shadmansaalim321 <shadmansaalim321@gmail.com>
2022-02-13 21:09:15 +1100

    Project adding with Laravel along with React for Backend

---
+++ resources/js/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+    return (
+        <>
+
+        </>
+    );
+}
+
+export default App;
+
+if (document.getElementById('app')) {
+    ReactDOM.render(<App />, document.getElementById('app'));
+}
